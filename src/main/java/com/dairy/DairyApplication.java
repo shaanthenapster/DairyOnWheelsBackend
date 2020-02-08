4a741ac90a793b9476aa1fb818dd850b70e7cd4f
shaanthenapster <shaanthenapster@gmail.com>
2020-02-09 00:18:04 +0530

    initial files for commit
    Signed-off-by: shaanthenapster <shaanthenapster@gmail.com>

--- src/main/java/com/dairy/DairyApplication.java
+++ src/main/java/com/dairy/DairyApplication.java
@@ -1,9 +1,12 @@
 package com.dairy;
 
+import com.dairy.configuration.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class DairyApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/dairy/repository/UserRepo.java
+++ src/main/java/com/dairy/repository/UserRepo.java
@@ -4,9 +4,16 @@ import com.dairy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepo extends JpaRepository<User , Long> {
 
     User findByContact(String mobileno);
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
 
 }
